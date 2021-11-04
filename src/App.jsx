60f9c68e6d6dc59cd0996614f37beced86a0fcaf
JosielMatos <josielmatos77@protonmail.com>
2021-11-04 09:29:42 -0300

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return (
-    <section>
-      <RepositoryList />
-      <Counter />
-    </section>
-  );
+  return <RepositoryList />;
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss";
+
+//https://api.github.com/users/JosielMatos/repos
 
 const repository = {
-    name: "robofriends",
-    description: "search for your robot friends",
-    link: "https://github.com/JosielMatos/robofriends"
-}
+  name: "robofriends",
+  description: "search for your robot friends",
+  link: "https://github.com/JosielMatos/robofriends",
+};
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
   return (
-    <section className='wrapper'>
+    <section className='repositoriesWrapper'>
       <h1>Lista de repositórios</h1>
 
       <ul>
